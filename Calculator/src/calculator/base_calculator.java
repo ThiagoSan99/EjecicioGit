270b38f7c2a53862fa1f77088db31e2f480bfe9d
David Enriquez <iemccp7.5enriquezdavidmail.com>
2023-10-09 23:05:46 -0500

    Cambio en calculadoras

---
+++ Calculator/src/calculator/base_calculator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hewlett Packard
+ */
+public class base_calculator {
+    Scanner leer = new Scanner(System.in);
+    public double substract(){
+        
+        System.out.println(" Digite dos numeros ");
+        double b = leer.nextDouble();
+        double a = leer.nextDouble();
+        
+        return b = (b - a);
+    }
+    public double adition(){
+        
+        System.out.println(" Digite dos numeros ");
+        double b = leer.nextDouble();
+        double a = leer.nextDouble();
+        
+        return b = (b + a);
+    }
+    
+}
